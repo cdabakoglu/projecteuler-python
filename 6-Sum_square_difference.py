c91a3ea50e8ea65b6cc67ed1088ab056a042fdc2
Caner Dabakoğlu <43098300+cdabakoglu@users.noreply.github.com>
2018-10-17 23:55:59 +0300

    Create 6-Sum_square_difference.py

---
+++ 6-Sum_square_difference.py
@@ -0,0 +1,14 @@
+sumOfSquare = 0
+squareOfSum = 0
+
+for i in range (1,101):
+    sumOfSquare += i ** 2
+    squareOfSum += i
+
+
+print(squareOfSum**2 - sumOfSquare)
+
+
+
+# Caner Dabakoğlu
+# GitHub: https://github.com/cdabakoglu
